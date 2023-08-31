e2b32f1471a0a766e03dc3adbd8916f377300698
Faiyaz Raza <iamfaiyazraza1106@gmail.com>
2023-09-01 00:33:02 +0530

    day28

---
+++ day17/day17.py
@@ -0,0 +1,16 @@
+# class User:
+#     pass
+#
+# user_1 = User()
+# user_1.id = "001"
+# user_1.username = "faiyaz"
+#
+# print(user_1.username)
+#
+
+class User:
+    def __int__(self):
+        print("New user is being created...")
+
+
+user_1 = User()
